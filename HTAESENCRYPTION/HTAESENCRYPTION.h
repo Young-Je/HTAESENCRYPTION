d9829dea932dc765400a8f3a6c022920a4e80c13
YoungJeXu <youngjexu@icloud.com>
2018-01-17 09:46:07 +0800

    Initial Commit

---
+++ HTAESENCRYPTION/HTAESENCRYPTION.h
@@ -0,0 +1,13 @@
+//
+//  HTAESENCRYPTION.h
+//  HTAESENCRYPTION
+//
+//  Created by Hutong on 17/01/2018.
+//  Copyright © 2018 Hutong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTAESENCRYPTION : NSObject
+
+@end
---
+++ HTAESENCRYPTION/HTAESENCRYPTION.m
@@ -0,0 +1,13 @@
+//
+//  HTAESENCRYPTION.m
+//  HTAESENCRYPTION
+//
+//  Created by Hutong on 17/01/2018.
+//  Copyright © 2018 Hutong. All rights reserved.
+//
+
+#import "HTAESENCRYPTION.h"
+
+@implementation HTAESENCRYPTION
+
+@end
